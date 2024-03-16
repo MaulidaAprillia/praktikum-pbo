903941721f8d8c9d14a54a265f567a91c8ba918a
MaulidaAprillia <82227765+MaulidaAprillia@users.noreply.github.com>
2024-03-16 21:14:42 +0700

    Add files via upload

---
+++ Tugas Standar/org/main/MPoligon.java
@@ -0,0 +1,20 @@
+//Nama      : Maulida Aprillia Cinta Ariyatin
+//NIM       : 24060122120029
+//Deskripsi :  driver class untuk poligon dan persegi panjang
+//Tanggal   : 16 Maret 2024
+
+package org.main;
+
+import org.bangundatar.*;
+
+public class MPoligon {
+    public static void main(String[] args){
+        PersegiPanjang persegi = new PersegiPanjang(10, 5, 4);
+        persegi.printInfo();
+        System.out.println("Luas Persegi Panjang : " +persegi.hitungLuas());
+
+        Segitiga segitiga = new Segitiga(2, 3, 4);
+        segitiga.printInfo();;
+        System.out.println("Luas Segitiga : " +segitiga.hitungLuas());
+    }
+}
